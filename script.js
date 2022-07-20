c846a4a46e59aec7c371840de9042c020aeeec5f
nenebeji <105363883+nenebeji@users.noreply.github.com>
2022-07-20 20:02:29 +0100

    updated generate password function

--- script.js
+++ script.js
@@ -1,76 +1,100 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// Generate prompt messages and alerts
-function generatePassword() {
-    var Length = window.prompt ("How long would you like your password? It has to be between 8 to 128 characters long");
-    
-    if (!Length) {
-        return;
-    }
+// Variables for Characters   
+var uppercase = "QWERTYUIOPASDFGHJKLZXCVBNM";
+var lowercase = "qwertyuiopasdfghjklzxcvbnm";
+var numbers = "1234567890";
+var symbols = "!@£$%^&*()_+-=[]{}\';./,?><:|";
 
-    else if (Length > 128 || Length < 8) {
-        window.alert ("Password length has to be between 8 to 128 characters long");
-    }
+var possibleCharacters = "";
 
-    else {
-        var uppercase = window.confirm ("Click ok to include atleast one uppercase character");
-    }
+// Variables to store information from the prompt input
+var CharacterLength = '';
+var uppercaseprompt = '';
+var lowercaseprompt = '';
+var numbersprompt = '';
+var symbolsprompt = '';
 
-    var lowercase = window.confirm ("Click ok to include atleast one lowercase character");
-
-    var numeric = window.confirm ("Click ok to include atleast one numeric character");
-
-    var special = window.confirm ("Click ok to include atleast one special character");
-
-    var validate = window.confirm ("Click Ok to confirm your selections");
-
-    return RandomGenerator;
-}
-
-// Code generator
-
-var RandomGenerator = {
-    Upper: RandomUppercase(),
-    Lower: RandomLowercase(),
-    Numeric: +RandomNumeric(),
-    Special: RandomSpecial()
-    
-};
-
-for (var i = 0; i < 128; i++) {
-    let randomNumber = Math.floor(Math.random() * RandomGenerator.length);
-
-    console.log (randomNumber);
-}
-
-// Math floor to make it a whole number and pick at random from the 26 letters in the alphabeth. 97 from Charset lowercase
-function RandomUppercase() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function RandomLowercase() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function RandomNumeric() {
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-// Creates a string of symbols and selects randomly from that string
-function RandomSpecial() {
-    var special = "*@!£$%&^()_+{}[];'>.,?/|§"
-    return special[Math.floor(Math.random() * special.length)];
-}
- 
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
+
   passwordText.value = password;
 
 }
 
+function generatePassword() {
+ // windows prompt when generate password button is clicked
+  var Length = + window.prompt ("How long would you like your password? It has to be between 8 to 128 characters long");
+  
+  if (!Length) {
+      return;
+  }
+
+  else if (Length > 128 || Length < 8) {
+      window.alert ("Password length has to be between 8 to 128 characters long");
+      return;
+  }
+
+  else (CharacterLength = Length);
+  
+  var uppercaseprompt = confirm ("Click ok to include atleast one uppercase character");
+
+
+  var lowercaseprompt = confirm ("Click ok to include atleast one lowercase character");
+
+
+
+  var numbersprompt = confirm ("Click ok to include atleast one numeric character");
+
+  
+
+  var symbolsprompt = confirm ("Click ok to include atleast one special character");
+
+
+  var validate = confirm ("Click Ok to confirm your selections");
+
+  console.log(Length);
+
+ // Declaring the values to include in possibleCharacters 
+
+  if(uppercaseprompt) {
+    possibleCharacters = possibleCharacters + uppercase;
+    console.log(possibleCharacters);
+  }
+
+  if(lowercaseprompt) {
+    possibleCharacters = possibleCharacters + lowercase;
+    console.log (possibleCharacters);
+  }
+
+  if(numbersprompt) {
+    possibleCharacters = possibleCharacters + numbers;
+    console.log (possibleCharacters);
+  }
+
+  if(symbolsprompt) {
+    possibleCharacters = possibleCharacters + symbols;
+    console.log (possibleCharacters);
+  }
+
+  if(!validate) {
+    return;
+  }
+ 
+  var password = '';
+ 
+  for(var i= 0; i < CharacterLength; i++){
+    var randomNum = Math.floor(Math.random() * possibleCharacters.length);
+    console.log(randomNum)
+
+    password += possibleCharacters[randomNum];
+  }
+
+  return password;
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
